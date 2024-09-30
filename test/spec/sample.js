5a3d93473d858aa63ffab0f52bfd25adcc503fc4
qameta-ci <qameta-ci@qameta.io>
2024-09-30 16:15:45 +0000

    update 2024-09-30T16:15:45

--- test/spec/sample.js
+++ test/spec/sample.js
@@ -2,18 +2,18 @@ const { it } = require("mocha")
 const { label, link, issue, tms, step, attachment } = require("allure-js-commons");
 
 it("sample test", async () => {
-	await label("foo", "bar");
-	await link("https://allurereport.org", "custom", "Allure Web-Site");
-	await issue("https://allurereport.org/fake-issue/1", "Issue 1");
-	await issue("2", "Issue 2");
-	await tms("https://allurereport.org/fake-task/1", "Task 1");
-	await tms("2", "Task 2");
-	await attachment("Sample text attachment", "Hello world!", "text/plain");
-	await step("step 1", async () => {
-		await step("step 1.1", async () => {
-			await step("step 1.1.1", async () => {
-				await attachment("Sample text attachment for step", "Goodbye world!", "text/plain");
-			});
-		});
-	});
-})
+  await label("foo", "bar");
+  await link("https://allurereport.org", "custom", "Allure Web-Site");
+  await issue("https://allurereport.org/fake-issue/1", "Issue 1");
+  await issue("2", "Issue 2");
+  await tms("https://allurereport.org/fake-task/1", "Task 1");
+  await tms("2", "Task 2");
+  await attachment("Sample text attachment", "Hello world!", "text/plain");
+  await step("step 1", async () => {
+    await step("step 1.1", async () => {
+      await step("step 1.1.1", async () => {
+        await attachment("Sample text attachment for step", "Goodbye world!", "text/plain");
+      });
+    });
+  });
+});
